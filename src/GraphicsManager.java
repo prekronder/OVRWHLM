0d72989256d33c4f93b211a10db94a1577651c61
prekronder <prekronder@gmail.com>
2024-03-18 11:19:19 -0400

    Initial Commit

---
+++ src/GraphicsManager.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class GraphicsManager {
+
+    public static void openUI() {
+        JFrame frame = new JFrame();
+        frame.setSize(700, 500);
+        frame.setVisible(true);
+    }
+
+}
